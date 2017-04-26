c78a2d8e6310027686b5f92f0dc7071362a3b683
paulphoenix01 <paulphoenix01@gmail.com>
2017-04-26 09:20:49 -0700

    CHanged neutron config, added dhcp and metadata to compute

--- install/install_neutron.sh
+++ install/install_neutron.sh
@@ -60,8 +60,7 @@ EOF
 	echocolor "Install NEUTRON node - Using Linux Bridge on $1"
 	sleep 5
 	apt-get -y install neutron-server neutron-plugin-ml2 \
-	neutron-linuxbridge-agent neutron-l3-agent neutron-dhcp-agent \
-	neutron-metadata-agent
+	neutron-linuxbridge-agent neutron-l3-agent
 
 
 	######## Backup configuration NEUTRON.CONF ##################"
@@ -83,6 +82,7 @@ EOF
 	# ops_edit $neutron_ctl DEFAULT nova_url http://$CTL_MGNT_IP:8774/v2
 	# ops_edit $neutron_ctl DEFAULT verbose True
 	ops_edit $neutron_ctl DEFAULT transport_url  rabbit://openstack:$RABBIT_PASS@$CTL_MGNT_IP
+	ops_edit $neutron_ctl DEFAULT dhcp_agents_per_network = 2
 
 	## [database] section
 	ops_edit $neutron_ctl database \
@@ -131,12 +131,12 @@ EOF
 
 	## [ml2_type_flat] section
 	ops_edit $ml2_clt ml2_type_flat flat_networks provider
-	ops_edit $ml2_clt ml2_type_vlan network_vlan_ranges vlan:20:50
+	ops_edit $ml2_clt ml2_type_vlan network_vlan_ranges provider
 	## [ml2_type_gre] section
 	# ops_edit $ml2_clt ml2_type_gre tunnel_id_ranges 100:200
 
 	## [ml2_type_vxlan] section
-	ops_edit $ml2_clt ml2_type_vxlan vni_ranges 201:300
+	#ops_edit $ml2_clt ml2_type_vxlan vni_ranges 201:300
 
 	## [securitygroup] section
 	ops_edit $ml2_clt securitygroup enable_ipset True
@@ -146,12 +146,12 @@ EOF
 	test -f $lbfile.orig || cp $lbfile $lbfile.orig
 
 	# [linux_bridge] section
-	ops_edit $lbfile linux_bridge physical_interface_mappings provider:$EXT_INTERFACE,vlan:$MGNT_INTERFACE
+	ops_edit $lbfile linux_bridge physical_interface_mappings provider:$EXT_INTERFACE
 
 	# [vxlan] section
-	ops_edit $lbfile vxlan enable_vxlan True
-	ops_edit $lbfile vxlan local_ip $CTL_DATA_IP
-	ops_edit $lbfile vxlan l2_population True
+	#ops_edit $lbfile vxlan enable_vxlan True
+	#ops_edit $lbfile vxlan local_ip $CTL_DATA_IP
+	#ops_edit $lbfile vxlan l2_population True
 
 	# [securitygroup] section
 	ops_edit $lbfile securitygroup enable_security_group True
@@ -171,35 +171,6 @@ EOF
 	# ops_edit $netl3agent DEFAULT verbose True
 
 
-	echocolor "Configuring DHCP AGENT"
-	sleep 7
-	#
-	test -f $netdhcp.orig || cp $netdhcp $netdhcp.orig
-
-	## [DEFAULT] section
-	ops_edit $netdhcp DEFAULT interface_driver \
-	    neutron.agent.linux.interface.BridgeInterfaceDriver
-	ops_edit $netdhcp DEFAULT dhcp_driver neutron.agent.linux.dhcp.Dnsmasq
-	ops_edit $netdhcp DEFAULT enable_isolated_metadata True
-	ops_edit $netdhcp DEFAULT dnsmasq_config_file /etc/neutron/dnsmasq-neutron.conf
-
-
-	echocolor "Config MTU"
-	sleep 3
-	echo "dhcp-option-force=26,1454" > /etc/neutron/dnsmasq-neutron.conf
-	# killall dnsmasq
-
-	echocolor "Configuring METADATA AGENT"
-	sleep 7
-	
-
-	test -f $netmetadata.orig || cp $netmetadata $netmetadata.orig
-
-	## [DEFAULT]
-	ops_edit $netmetadata DEFAULT nova_metadata_ip $CTL_MGNT_IP
-	ops_edit $netmetadata DEFAULT metadata_proxy_shared_secret $METADATA_SECRET
-
-
 	su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
 	    --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
 
@@ -213,8 +184,6 @@ EOF
 	sleep 7
 	service neutron-server restart
 	service neutron-linuxbridge-agent restart
-	service neutron-dhcp-agent restart
-	service neutron-metadata-agent restart
 	service neutron-l3-agent restart
 
 	rm -f /var/lib/neutron/neutron.sqlite
@@ -227,7 +196,7 @@ EOF
 elif [ "$1" == "compute1" ]; then
 	echocolor "Restarting NEUTRON on $1"
 	sleep 3
-	apt -y install neutron-linuxbridge-agent
+	apt -y install neutron-linuxbridge-agent neutron-dhcp-agent neutron-metadata-agent
 	test -f $neutron_com.orig || cp $neutron_com $neutron_com.orig
 
 	ops_edit $neutron_com DEFAULT transport_url  rabbit://openstack:$RABBIT_PASS@$CTL_MGNT_IP
@@ -267,15 +236,46 @@ elif [ "$1" == "compute1" ]; then
 	ops_edit $lbfile securitygroup enable_security_group True
 	ops_edit $lbfile securitygroup firewall_driver \
 	    neutron.agent.linux.iptables_firewall.IptablesFirewallDriver
+	
+	echocolor "Configuring DHCP AGENT"
+        sleep 7
+        #
+        test -f $netdhcp.orig || cp $netdhcp $netdhcp.orig
 
+        ## [DEFAULT] section
+        ops_edit $netdhcp DEFAULT interface_driver \
+            neutron.agent.linux.interface.BridgeInterfaceDriver
+        ops_edit $netdhcp DEFAULT dhcp_driver neutron.agent.linux.dhcp.Dnsmasq
+        ops_edit $netdhcp DEFAULT enable_isolated_metadata True
+        #ops_edit $netdhcp DEFAULT dnsmasq_config_file /etc/neutron/dnsmasq-neutron.conf
+
+
+        #echocolor "Config MTU"
+        #sleep 3
+        #echo "dhcp-option-force=26,1454" > /etc/neutron/dnsmasq-neutron.conf
+        # killall dnsmasq
+
+        echocolor "Configuring METADATA AGENT"
+        sleep 7
+
+
+        test -f $netmetadata.orig || cp $netmetadata $netmetadata.orig
+
+        ## [DEFAULT]
+        ops_edit $netmetadata DEFAULT nova_metadata_ip $CTL_MGNT_IP
+        ops_edit $netmetadata DEFAULT metadata_proxy_shared_secret $METADATA_SECRET
+
+	
 	echocolor "Restarting NEUTRON service "
 	sleep 7
 	service neutron-linuxbridge-agent restart
+	service neutron-dhcp-agent restart
+        service neutron-metadata-agent restart
 
 elif [ "$1" == "compute2" ]; then
 	echocolor "Restarting NEUTRON on $1"
 	sleep 3
-	apt -y install neutron-linuxbridge-agent
+	apt -y install neutron-linuxbridge-agent neutron-dhcp-agent neutron-metadata-agent
 	test -f $neutron_com.orig || cp $neutron_com $neutron_com.orig
 
 	ops_edit $neutron_com DEFAULT transport_url  rabbit://openstack:$RABBIT_PASS@$CTL_MGNT_IP
@@ -304,21 +304,53 @@ elif [ "$1" == "compute2" ]; then
 	test -f $lbfile.orig || cp $lbfile $lbfile.orig
 
 	# [linux_bridge] section
-	ops_edit $lbfile linux_bridge physical_interface_mappings provider:$EXT_INTERFACE,vlan:$MGNT_INTERFACE
+	ops_edit $lbfile linux_bridge physical_interface_mappings provider:$EXT_INTERFACE
 
 	# [vxlan] section
-	ops_edit $lbfile vxlan enable_vxlan True
-	ops_edit $lbfile vxlan local_ip $COM2_DATA_IP
-	ops_edit $lbfile vxlan l2_population True
+	#ops_edit $lbfile vxlan enable_vxlan True
+	#ops_edit $lbfile vxlan local_ip $COM2_DATA_IP
+	#ops_edit $lbfile vxlan l2_population True
 
 	# [securitygroup] section
 	ops_edit $lbfile securitygroup enable_security_group True
 	ops_edit $lbfile securitygroup firewall_driver \
 	    neutron.agent.linux.iptables_firewall.IptablesFirewallDriver
 
+        echocolor "Configuring DHCP AGENT"
+        sleep 7
+        #
+        test -f $netdhcp.orig || cp $netdhcp $netdhcp.orig
+
+        ## [DEFAULT] section
+        ops_edit $netdhcp DEFAULT interface_driver \
+            neutron.agent.linux.interface.BridgeInterfaceDriver
+        ops_edit $netdhcp DEFAULT dhcp_driver neutron.agent.linux.dhcp.Dnsmasq
+        ops_edit $netdhcp DEFAULT enable_isolated_metadata True
+        #ops_edit $netdhcp DEFAULT dnsmasq_config_file /etc/neutron/dnsmasq-neutron.conf
+
+
+        #echocolor "Config MTU"
+        #sleep 3
+        #echo "dhcp-option-force=26,1454" > /etc/neutron/dnsmasq-neutron.conf
+        # killall dnsmasq
+
+        echocolor "Configuring METADATA AGENT"
+        sleep 7
+
+
+        test -f $netmetadata.orig || cp $netmetadata $netmetadata.orig
+
+        ## [DEFAULT]
+        ops_edit $netmetadata DEFAULT nova_metadata_ip $CTL_MGNT_IP
+        ops_edit $netmetadata DEFAULT metadata_proxy_shared_secret $METADATA_SECRET
+
+
 	echocolor "Restarting NEUTRON service "
 	sleep 7
 	service neutron-linuxbridge-agent restart	
+	service neutron-dhcp-agent restart
+        service neutron-metadata-agent restart
+
 
 else
 	echocolor "Khong phai node controller"
